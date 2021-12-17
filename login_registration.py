0b7bbda3b1b2e06e07a102faa8c85cb54d3e9f63
VasVladmir <volgodoc@yandex.ru>
2021-12-17 15:13:36 +0300

    Победил регистрацию без тормозов на зеленом пароле

--- login_registration.py
+++ login_registration.py
@@ -15,10 +15,14 @@ time.sleep(1)
 # Вкладка My Acc-nt Mn - регистрация: почта - пароль - Input
 MyAccnt_UpMn = driver.find_element_by_link_text("My Account")
 MyAccnt_UpMn.click()
-Mail_reg_Area = driver.find_element_by_id("reg_email").send_keys("111retrapakko@vusra.com")
-Pass_reg_Area = driver.find_element_by_id("reg_password").send_keys("111tap@rj219011IoE_")
-time.sleep(3)
-Reg_btn = driver.find_element_by_css_selector('input[value="Register"]').click()
+time.sleep(1)
+Mail_reg_Area = driver.find_element_by_id("reg_email").send_keys("121retrapakko@vusra.com")
+Pass_reg_Area = driver.find_element_by_id("reg_password").send_keys("121tap@rj219011IoE_")
+time.sleep(2)
+explicit_20 = WebDriverWait(driver, 20)
+Reg_btn_EC = explicit_20.until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[value="Register"]'))).click()
+# Reg_btn = driver.find_element_by_css_selector('input[value="Register"]').click()
 time.sleep(2)
 
 # Разлогин после регистрации , что бы отработать залогин в след слайде
@@ -32,7 +36,7 @@ time.sleep(1)
 Logn_btn = driver.find_element_by_css_selector('input[value="Login"]').click()
 
 # Проверка элемента Logout на странице
-explicit_20 = WebDriverWait(driver, 20)
+# explicit_20 = WebDriverWait(driver, 20)
 Logout_lnk = explicit_20.until(
     EC.visibility_of_element_located((By.LINK_TEXT, "Logout")))
 if Logout_lnk is None: # если элемента НЕТ
